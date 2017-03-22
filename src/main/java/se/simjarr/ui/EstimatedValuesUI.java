d5d9d803545eb1a1f7ef565f93859d21b38abc71
Dewin <simon@temtron.se>
2017-03-22 18:41:53 +0100

    styled the ratio and changed GridLayout to 5, 7 instead of 3,7

--- src/main/java/se/simjarr/ui/EstimatedValuesUI.java
+++ src/main/java/se/simjarr/ui/EstimatedValuesUI.java
@@ -8,8 +8,6 @@ import se.simjarr.model.HttpRequestHandler;
 import se.simjarr.model.TradeOffer;
 
 import java.math.BigDecimal;
-import java.math.MathContext;
-import java.math.RoundingMode;
 import java.util.List;
 
 import static se.simjarr.global.GlobalVariables.HC_LEGACY;
@@ -24,7 +22,7 @@ public class EstimatedValuesUI extends GridLayout {
         this.setWidth("220px");
         this.setHeight("320px");
         this.setMargin(true);
-        generateMatrixGrid(3, 7);
+        generateMatrixGrid(5, 7);
 
         addEstimatedValuesSection();
     }
@@ -33,9 +31,10 @@ public class EstimatedValuesUI extends GridLayout {
         for (int i = 0; i <= 6; i++) {
             ((Label) this.getComponent(0, i)).setValue("<img style='width:30px; height:30px;'src='http://currency.poe.trade/static/currency/Chaos_Orb.png'/>");
             if (i != REFERENCE_CURRENCY.getIntValue() - 1) {
-                ((Label) this.getComponent(1, i)).setValue(String.valueOf(calcEstimatedValue(Currency.fromValue(i), 5)));
+                calcEstimatedValue(Currency.fromValue(i), 5, i);
             }
-            ((Label) this.getComponent(2, i)).setValue("<img style='width:30px; height:30px;'src='http://currency.poe.trade/static/currency/Orb_of_Alteration.png'/>");
+            ((Label) this.getComponent(2, i)).setValue(" : ");
+            ((Label) this.getComponent(4, i)).setValue("<img style='width:30px; height:30px;'src='http://currency.poe.trade/static/currency/Orb_of_Alteration.png'/>");
         }
     }
 
@@ -46,19 +45,17 @@ public class EstimatedValuesUI extends GridLayout {
 
         for (int row = 0; row < this.getRows(); row++) {
             for (int col = 0; col < this.getColumns(); col++) {
-                final Label child = new Label("NOP", ContentMode.HTML);
+                final Label child = new Label("N/A", ContentMode.HTML);
                 this.addComponent(child);
                 this.setComponentAlignment(child, Alignment.MIDDLE_CENTER);
-                this.setRowExpandRatio(row, 0.0f);
-                this.setColumnExpandRatio(col, 0.0f);
             }
         }
     }
 
     //TODO: fetch estimated value for currency compared to selected reference_currency
-    private double calcEstimatedValue(Currency currency, int size) {
-        List<TradeOffer> trades = fetchTrades(REFERENCE_CURRENCY, currency, size);
-        trades.addAll(fetchTrades(currency, REFERENCE_CURRENCY, size));
+    private void calcEstimatedValue(Currency currency, int sampleSize, int row) {
+        List<TradeOffer> trades = fetchTrades(REFERENCE_CURRENCY, currency, sampleSize);
+        trades.addAll(fetchTrades(currency, REFERENCE_CURRENCY, sampleSize));
 
         double estimatedValue = 0;
         for (TradeOffer tradeOffer : trades) {
@@ -66,9 +63,7 @@ public class EstimatedValuesUI extends GridLayout {
         }
 
         estimatedValue /= trades.size();
-        estimatedValue = new BigDecimal(estimatedValue).setScale(3, BigDecimal.ROUND_HALF_UP).doubleValue();
-
-        return estimatedValue;
+        ratioFixerAndPrinter(estimatedValue, row);
     }
 
     private List<TradeOffer> fetchTrades(Currency fromCurrency, Currency toCurrency, int size) {
@@ -76,5 +71,22 @@ public class EstimatedValuesUI extends GridLayout {
         String requestUrl = urlBuilder.setHave(fromCurrency).setWant(toCurrency).build();
 
         return HttpRequestHandler.fetchTradesFromUrl(requestUrl, size);
+    }
+
+    private void ratioFixerAndPrinter(double value, int row){
+
+
+        if (value < 1) {
+            ((Label)this.getComponent(1, row)).setValue(new BigDecimal(1 / value).setScale(3, BigDecimal.ROUND_HALF_UP).toString());
+            this.setComponentAlignment(this.getComponent(1, row), Alignment.MIDDLE_RIGHT);
+            ((Label)this.getComponent(3, row)).setValue("1");
+            this.setComponentAlignment(this.getComponent(3, row), Alignment.MIDDLE_LEFT);
+
+        } else {
+            ((Label)this.getComponent(1, row)).setValue("1");
+            this.setComponentAlignment(this.getComponent(1, row), Alignment.MIDDLE_RIGHT);
+            ((Label)this.getComponent(3, row)).setValue(new BigDecimal(1 / value).setScale(3, BigDecimal.ROUND_HALF_UP).toString());
+            this.setComponentAlignment(this.getComponent(3, row), Alignment.MIDDLE_LEFT);
+        }
     }
 }
