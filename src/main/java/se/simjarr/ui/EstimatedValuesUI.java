0b753d07e2ed41962d0c8f7fbed28c718ec9da5e
Dewin <simon@temtron.se>
2017-03-22 17:54:11 +0100

    fixed a precision for the values

--- src/main/java/se/simjarr/ui/EstimatedValuesUI.java
+++ src/main/java/se/simjarr/ui/EstimatedValuesUI.java
@@ -7,6 +7,9 @@ import se.simjarr.model.CurrencyTradeUrlBuilder;
 import se.simjarr.model.HttpRequestHandler;
 import se.simjarr.model.TradeOffer;
 
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.math.RoundingMode;
 import java.util.List;
 
 import static se.simjarr.global.GlobalVariables.HC_LEGACY;
@@ -26,11 +29,13 @@ public class EstimatedValuesUI extends GridLayout {
         addEstimatedValuesSection();
     }
 
-    private void addEstimatedValuesSection(){
-        for (int i = 0; i <= 6; i++){
-            ((Label)this.getComponent(0, i)).setValue("<img style='width:30px; height:30px;'src='http://currency.poe.trade/static/currency/Chaos_Orb.png'/>");
-            ((Label)this.getComponent(1, i)).setValue(String.valueOf(calcEstimatedValue(Currency.fromValue(i), 5)));
-            ((Label)this.getComponent(2, i)).setValue("<img style='width:30px; height:30px;'src='http://currency.poe.trade/static/currency/Orb_of_Alteration.png'/>");
+    private void addEstimatedValuesSection() {
+        for (int i = 0; i <= 6; i++) {
+            ((Label) this.getComponent(0, i)).setValue("<img style='width:30px; height:30px;'src='http://currency.poe.trade/static/currency/Chaos_Orb.png'/>");
+            if (i != REFERENCE_CURRENCY.getIntValue() - 1) {
+                ((Label) this.getComponent(1, i)).setValue(String.valueOf(calcEstimatedValue(Currency.fromValue(i), 5)));
+            }
+            ((Label) this.getComponent(2, i)).setValue("<img style='width:30px; height:30px;'src='http://currency.poe.trade/static/currency/Orb_of_Alteration.png'/>");
         }
     }
 
@@ -41,7 +46,7 @@ public class EstimatedValuesUI extends GridLayout {
 
         for (int row = 0; row < this.getRows(); row++) {
             for (int col = 0; col < this.getColumns(); col++) {
-                final Label child = new Label("1:17", ContentMode.HTML);
+                final Label child = new Label("NOP", ContentMode.HTML);
                 this.addComponent(child);
                 this.setComponentAlignment(child, Alignment.MIDDLE_CENTER);
                 this.setRowExpandRatio(row, 0.0f);
@@ -56,19 +61,20 @@ public class EstimatedValuesUI extends GridLayout {
         trades.addAll(fetchTrades(currency, REFERENCE_CURRENCY, size));
 
         double estimatedValue = 0;
-        for(TradeOffer tradeOffer : trades) {
+        for (TradeOffer tradeOffer : trades) {
             estimatedValue += tradeOffer.getReferenceRatio();
         }
 
         estimatedValue /= trades.size();
+        estimatedValue = new BigDecimal(estimatedValue).setScale(3, BigDecimal.ROUND_HALF_UP).doubleValue();
 
         return estimatedValue;
     }
 
     private List<TradeOffer> fetchTrades(Currency fromCurrency, Currency toCurrency, int size) {
         CurrencyTradeUrlBuilder urlBuilder = new CurrencyTradeUrlBuilder(HC_LEGACY, true);
-        String reguestUrl = urlBuilder.setHave(fromCurrency).setWant(toCurrency).build();
+        String requestUrl = urlBuilder.setHave(fromCurrency).setWant(toCurrency).build();
 
-        return HttpRequestHandler.fetchTradesFromUrl(reguestUrl, size);
+        return HttpRequestHandler.fetchTradesFromUrl(requestUrl, size);
     }
 }
