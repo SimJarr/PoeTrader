7fdcf435b969cb09744812dd74a058a11de86516
jaccohub <jacoblodenius@gmail.com>
2017-03-24 00:47:37 +0100

    added layout that displays profitable trades

--- src/main/java/se/simjarr/ui/TradeFinderLayout.java
+++ src/main/java/se/simjarr/ui/TradeFinderLayout.java
@@ -5,17 +5,25 @@ import com.vaadin.shared.ui.slider.SliderOrientation;
 import com.vaadin.ui.*;
 import se.simjarr.global.Currency;
 import se.simjarr.model.TradeFinder;
+import se.simjarr.model.TradeOffer;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class TradeFinderLayout extends VerticalLayout {
 
+    private Accordion tradeDisplayLayout;
+    private TextField minProfitInput;
     private Map<Currency, String> currencyId;
+    private double minProfitPerTrade;
 
     public TradeFinderLayout() {
+        tradeDisplayLayout = new Accordion();
         currencyId = new HashMap<>();
+        minProfitPerTrade = 0.1;
 
         addHeader();
         addCurrencySelection();
@@ -39,6 +47,18 @@ public class TradeFinderLayout extends VerticalLayout {
             currencyId.put(current, slider.getId());
             formLayout.addComponent(slider);
         }
+
+        HorizontalLayout horizontalLayout = new HorizontalLayout();
+
+        //TODO: fix alignment
+        Label label = new Label();
+        label.setValue("MIN PROFIT PER TRADE:");
+        label.setDescription("profit in reference currency");
+
+        minProfitInput = new TextField();
+        minProfitInput.setWidth(50, Unit.PIXELS);
+        minProfitInput.setDescription("default value 0.1");
+
         Button sendButton = new Button("Send");
         sendButton.addClickListener(clickEvent -> {
             Map<Currency, Integer> myCurrency = new HashMap<>();
@@ -48,9 +68,33 @@ public class TradeFinderLayout extends VerticalLayout {
             });
             TradeFinder tradeFinder = new TradeFinder();
             tradeFinder.setAvailableCurrency(myCurrency);
-            tradeFinder.tradeChainer(1, null);
+            minProfitPerTrade = Double.parseDouble(minProfitInput.getValue());
+            addTradeChainDisplay(tradeFinder.tradeChainer(minProfitPerTrade, null));
         });
-        this.addComponents(formLayout, sendButton);
+
+        horizontalLayout.addComponents(label, minProfitInput, sendButton);
+        this.addComponents(formLayout, horizontalLayout);
+    }
+
+    private void addTradeChainDisplay(List<TradeOffer> trades) {
+        tradeDisplayLayout.removeAllComponents();
+        AtomicInteger counter = new AtomicInteger(1);
+        trades.forEach(trade -> {
+            TextArea textArea = new TextArea();
+            textArea.setEnabled(false);
+            textArea.setWidth(100, Unit.PERCENTAGE);
+            textArea.setValue(tradeToString(trade));
+            tradeDisplayLayout.addTab(textArea, "TRADE " + counter.getAndIncrement());
+        });
+        this.addComponent(tradeDisplayLayout);
+    }
+
+    private String tradeToString(TradeOffer trade) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Username: ").append(trade.getUsername());
+        sb.append("\n").append("You get: ").append(Currency.fromValue(trade.getSellCurrency()).name()).append(" x ").append(trade.getSellValue());
+        sb.append("\n").append("You pay: ").append(Currency.fromValue(trade.getBuyCurrency()).name()).append(" x ").append(trade.getBuyValue());
+        return sb.toString();
     }
 
     private static Component findComponentById(HasComponents root, String id) {
