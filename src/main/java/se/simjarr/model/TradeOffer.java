fede887e8d81099b9ad02c899727eb290b6d673f
jaccohub <jacoblodenius@gmail.com>
2017-03-23 22:59:37 +0100

    equals & hashcode implemented and changed type of some members from string to int/double

--- src/main/java/se/simjarr/model/TradeOffer.java
+++ src/main/java/se/simjarr/model/TradeOffer.java
@@ -1,50 +1,48 @@
 package se.simjarr.model;
 
+import se.simjarr.global.Currency;
+
 import static se.simjarr.global.GlobalVariables.REFERENCE_CURRENCY;
 
 public class TradeOffer {
 
     private String username;
-    private String sellCurrency;
-    private String sellValue;
-    private String buyCurrency;
-    private String buyValue;
+    private int sellCurrency;
+    private int sellValue;
+    private int buyCurrency;
+    private int buyValue;
     private String ign;
-    private String stock;
+    private int stock;
     private double referenceRatio;
 
     public TradeOffer(String username, String sellCurrency, String sellValue, String buyCurrency, String buyValue, String ign, String stock) {
         this.username = username;
-        this.sellCurrency = sellCurrency;
-        this.sellValue = sellValue;
-        this.buyCurrency = buyCurrency;
-        this.buyValue = buyValue;
+        this.sellCurrency = Integer.parseInt(sellCurrency);
+        this.sellValue = (int) Double.parseDouble(sellValue);
+        this.buyCurrency = Integer.parseInt(buyCurrency);
+        this.buyValue = (int) Double.parseDouble(buyValue);
         this.ign = ign;
-        this.stock = stock;
+        this.stock = (stock.equals("")) ? -1 : Integer.parseInt(stock);
         this.referenceRatio = calculateRatioComparedToReference();
-    }
-
-    public double getReferenceRatio(){
-        return referenceRatio;
     }
 
     public String getUsername() {
         return username;
     }
 
-    public String getSellCurrency() {
+    public int getSellCurrency() {
         return sellCurrency;
     }
 
-    public String getSellValue() {
+    public int getSellValue() {
         return sellValue;
     }
 
-    public String getBuyCurrency() {
+    public int getBuyCurrency() {
         return buyCurrency;
     }
 
-    public String getBuyValue() {
+    public int getBuyValue() {
         return buyValue;
     }
 
@@ -52,22 +50,52 @@ public class TradeOffer {
         return ign;
     }
 
-    public String getStock() {
+    public int getStock() {
         return stock;
     }
 
+    public double getReferenceRatio() {
+        return referenceRatio;
+    }
+
     //TODO: no good, we want a real one
-    private double calculateRatioComparedToReference(){
-
-        int sellCurrency = Integer.parseInt(this.sellCurrency);
-        double sellValue = Double.parseDouble(this.sellValue);
-        int buyCurrency = Integer.parseInt(this.buyCurrency);
-        double buyValue = Double.parseDouble(this.buyValue);
-
-        if (sellCurrency != REFERENCE_CURRENCY.getIntValue() && buyCurrency != REFERENCE_CURRENCY.getIntValue()){
+    private double calculateRatioComparedToReference() {
+        if (sellCurrency != REFERENCE_CURRENCY.getIntValue() && buyCurrency != REFERENCE_CURRENCY.getIntValue()) {
             return 0;
         }
+        return (sellCurrency == REFERENCE_CURRENCY.getIntValue()) ? (double)buyValue/(double)sellValue : (double)sellValue/(double)buyValue;
+    }
 
-        return (sellCurrency == REFERENCE_CURRENCY.getIntValue()) ? buyValue/sellValue : sellValue/buyValue;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        TradeOffer that = (TradeOffer) o;
+
+        if (sellCurrency != that.sellCurrency) return false;
+        if (sellValue != that.sellValue) return false;
+        if (buyCurrency != that.buyCurrency) return false;
+        if (buyValue != that.buyValue) return false;
+        if (stock != that.stock) return false;
+        if (Double.compare(that.referenceRatio, referenceRatio) != 0) return false;
+        if (username != null ? !username.equals(that.username) : that.username != null) return false;
+        return ign != null ? ign.equals(that.ign) : that.ign == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = username != null ? username.hashCode() : 0;
+        result = 31 * result + sellCurrency;
+        result = 31 * result + sellValue;
+        result = 31 * result + buyCurrency;
+        result = 31 * result + buyValue;
+        result = 31 * result + (ign != null ? ign.hashCode() : 0);
+        result = 31 * result + stock;
+        temp = Double.doubleToLongBits(referenceRatio);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
     }
 }
