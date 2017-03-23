8364218b714d9ee7cc0ad5bed0a94c6cd19ed3e6
jaccohub <jacoblodenius@gmail.com>
2017-03-23 22:54:57 +0100

    trades with decimal values as amount does not pass validation and will not get parsed

--- src/main/java/se/simjarr/model/HttpRequestHandler.java
+++ src/main/java/se/simjarr/model/HttpRequestHandler.java
@@ -16,9 +16,11 @@ public abstract class HttpRequestHandler {
             Document doc = Jsoup.connect(requestUrl).get();
             Elements trades = doc.select(".displayoffer");
             trades.forEach(tradeOffer -> {
-                TradeOffer offer = new TradeOffer(tradeOffer.attr("data-username"), tradeOffer.attr("data-sellcurrency"), tradeOffer.attr("data-sellvalue"),
-                        tradeOffer.attr("data-buycurrency"), tradeOffer.attr("data-buyvalue"), tradeOffer.attr("data-ign"), tradeOffer.attr("data-stock"));
-                tradeOffers.add(offer);
+                if (validateValue(tradeOffer.attr("data-buyvalue")) && validateValue(tradeOffer.attr("data-sellvalue"))) {
+                    TradeOffer offer = new TradeOffer(tradeOffer.attr("data-username"), tradeOffer.attr("data-sellcurrency"), tradeOffer.attr("data-sellvalue"),
+                            tradeOffer.attr("data-buycurrency"), tradeOffer.attr("data-buyvalue"), tradeOffer.attr("data-ign"), tradeOffer.attr("data-stock"));
+                    tradeOffers.add(offer);
+                }
             });
         } catch (IOException e) {
             e.printStackTrace();
@@ -28,9 +30,16 @@ public abstract class HttpRequestHandler {
 
     public static List<TradeOffer> fetchTradesFromUrl(String requestUrl, int size) {
         List<TradeOffer> trades = fetchTradesFromUrl(requestUrl);
-        if(trades.size() > size) {
+        if (trades.size() > size) {
             trades = trades.subList(0, size);
         }
         return trades;
+    }
+
+    private static boolean validateValue(String value) {
+        if(value.equals("")) return false;
+        int intValue = (int) Double.parseDouble(value);
+        double doubleValue = Double.parseDouble(value);
+        return !((doubleValue - intValue) > 0);
     }
 }
