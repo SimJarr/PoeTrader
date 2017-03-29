7de5fb73602972f01269083f92becb901c6cd54a
Dewin <simon@temtron.se>
2017-03-29 16:02:34 +0200

    the testing tab "Work in progress"

--- src/main/java/se/simjarr/ui/TestLayout.java
+++ src/main/java/se/simjarr/ui/TestLayout.java
@@ -3,7 +3,10 @@ package se.simjarr.ui;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.VerticalLayout;
 import se.simjarr.global.Currency;
-import se.simjarr.model.TradeBank;
+import se.simjarr.model.TradeFinder;
+import se.simjarr.model.TradeOffer;
+
+import java.util.List;
 
 public class TestLayout extends VerticalLayout {
 
@@ -16,6 +19,29 @@ public class TestLayout extends VerticalLayout {
     }
 
     private void testMethod() {
-        System.out.println(TradeBank.estimateValue(Currency.JEWELLERS_ORB, 5));
+        TradeFinder tradeFinder = new TradeFinder();
+        tradeFinder.setTestCurrencies();
+
+        List<List<TradeOffer>> listList = tradeFinder.tradeChainer(0.1);
+
+        listList.forEach(list -> {
+            System.out.println("--------");
+            for (TradeOffer offer : list) {
+                System.out.println("Buy: " + offer.getBuyValue() + " " + Currency.fromValue(offer.getBuyCurrency()));
+                System.out.println("Sell: " + offer.getSellValue() + " " + Currency.fromValue(offer.getSellCurrency()));
+                System.out.println("========");
+            }
+            System.out.println("--------");
+        });
+
+        System.out.println(listList.size());
+
+
+
+        /*for (TradeOffer offer : tradeFinder.generateTradeChain(0.1)){
+            System.out.println("Buy: " + offer.getBuyValue() + " " + Currency.fromValue(offer.getBuyCurrency()));
+            System.out.println("Sell: " + offer.getSellValue() + " " + Currency.fromValue(offer.getSellCurrency()));
+            System.out.println("========");
+        }*/
     }
 }
