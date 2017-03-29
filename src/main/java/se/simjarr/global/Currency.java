5f41d6603a4dd86b2b4619739b0f65a5ca406453
Dewin <simon@temtron.se>
2017-03-29 21:06:46 +0200

    added lowercase name to every currency

--- src/main/java/se/simjarr/global/Currency.java
+++ src/main/java/se/simjarr/global/Currency.java
@@ -7,29 +7,31 @@ import java.io.File;
 
 public enum Currency {
 
-    ORB_OF_ALTERATION(1, "Orb_of_Alteration.png"),
-    ORB_OF_FUSING(2, "Orb_of_Fusing.png"),
-    ORB_OF_ALCHEMY(3, "Orb_of_Alchemy.png"),
-    CHAOS_ORB(4, "Chaos_Orb.png"),
-    GEMCUTTERS_PRISM(5, "Gemcutter's_Prism.png"),
-    EXALTED_ORB(6, "Exalted_Orb.png"),
-    CHROMATIC_ORB(7, "Chromatic_Orb.png"),
-    JEWELLERS_ORB(8, "Jeweller's_Orb.png"),
-    ORB_OF_CHANCE(9, "Orb_of_Chance.png"),
-    CARTOGRAPHERS_CHISEL(10, "Cartographer's_Chisel.png"),
-    ORB_OF_SCOURING(11, "Orb_of_Scouring.png"),
-    BLESSED_ORB(12, "Blessed_Orb.png"),
-    ORB_OF_REGRET(13, "Orb_of_Regret.png"),
-    REGAL_ORB(14, "Regal_Orb.png"),
-    DIVINE_ORB(15, "Divine_Orb.png"),
-    VAAL_ORB(16, "Vaal_Orb.png");
+    ORB_OF_ALTERATION(1, "Orb_of_Alteration.png", "alteration"),
+    ORB_OF_FUSING(2, "Orb_of_Fusing.png", "fusing"),
+    ORB_OF_ALCHEMY(3, "Orb_of_Alchemy.png", "alchemy"),
+    CHAOS_ORB(4, "Chaos_Orb.png", "chaos"),
+    GEMCUTTERS_PRISM(5, "Gemcutter's_Prism.png", "gcp"),
+    EXALTED_ORB(6, "Exalted_Orb.png", "exalted"),
+    CHROMATIC_ORB(7, "Chromatic_Orb.png", "chrome"),
+    JEWELLERS_ORB(8, "Jeweller's_Orb.png", "jeweller\u0027s"),
+    ORB_OF_CHANCE(9, "Orb_of_Chance.png", "chance"),
+    CARTOGRAPHERS_CHISEL(10, "Cartographer's_Chisel.png", "chisel"),
+    ORB_OF_SCOURING(11, "Orb_of_Scouring.png", "scouring"),
+    BLESSED_ORB(12, "Blessed_Orb.png", "blessed"),
+    ORB_OF_REGRET(13, "Orb_of_Regret.png", "regret"),
+    REGAL_ORB(14, "Regal_Orb.png", "regal"),
+    DIVINE_ORB(15, "Divine_Orb.png", "divine"),
+    VAAL_ORB(16, "Vaal_Orb.png", "vaal");
 
     private int intValue;
     private String imgPath;
+    private String lowerCaseName;
 
-    Currency(int intValue, String imgPath) {
+    Currency(int intValue, String imgPath, String lowerCaseName) {
         this.intValue = intValue;
         this.imgPath = imgPath;
+        this.lowerCaseName = lowerCaseName;
     }
 
     public static Currency fromValue(int value) {
@@ -43,6 +45,10 @@ public enum Currency {
 
     public String getImgPath() {
         return imgPath;
+    }
+
+    public String getLowerCaseName() {
+        return lowerCaseName;
     }
 
     public int getIntValue() {
