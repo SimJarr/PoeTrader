ea509de7dd9e31e6be0e285a0091eceff8e9e15e
jaccohub <jacoblodenius@gmail.com>
2017-04-12 21:42:06 +0200

    added reset method and inventory

--- src/main/java/se/simjarr/global/GlobalVariables.java
+++ src/main/java/se/simjarr/global/GlobalVariables.java
@@ -2,14 +2,22 @@ package se.simjarr.global;
 import java.util.HashMap;
 import java.util.Map;
 
-import static se.simjarr.global.Currency.*;
+import static se.simjarr.global.Currency.CHAOS_ORB;
 
 public abstract class GlobalVariables {
 
     //TODO: create enum for leagues
     public static final String HC_LEGACY = "Hardcore+Legacy";
 
-    public static Currency REFERENCE_CURRENCY = CHAOS_ORB;
+    public static Map<Currency, Integer> INVENTORY;
 
-    public static Map<Currency, Double> ESTIMATED_VALUES = new HashMap<>();
+    public static Currency REFERENCE_CURRENCY;
+
+    public static Map<Currency, Double> ESTIMATED_VALUES;
+
+    public static void reset() {
+        INVENTORY = null;
+        REFERENCE_CURRENCY = CHAOS_ORB;
+        ESTIMATED_VALUES = new HashMap<>();
+    }
 }
