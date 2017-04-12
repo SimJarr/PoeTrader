fb9ba50f6ca2eb62177d9d602331b11d0dddfa71
jaccohub <jacoblodenius@gmail.com>
2017-04-12 21:40:47 +0200

    cookie related stuff moved to separate class

---
+++ src/main/java/se/simjarr/global/Cookies.java
@@ -0,0 +1,24 @@
+package se.simjarr.global;
+
+import com.vaadin.server.VaadinService;
+
+import javax.servlet.http.Cookie;
+
+public abstract class Cookies {
+
+    public static void createCookie(String name, String value) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setPath("/");
+        VaadinService.getCurrentResponse().addCookie(cookie);
+    }
+
+    public static Cookie getCookieByName(String name) {
+        Cookie[] cookies = VaadinService.getCurrentRequest().getCookies();
+        for (Cookie cookie : cookies) {
+            if (name.equals(cookie.getName())) {
+                return cookie;
+            }
+        }
+        return null;
+    }
+}
