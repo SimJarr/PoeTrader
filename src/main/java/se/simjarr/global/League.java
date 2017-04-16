acb3d7083b72db5e3052d38f2da4c17cb4b11703
jaccohub <jacoblodenius@gmail.com>
2017-04-16 16:14:22 +0200

    fixed typo & added method to get league from displayname

--- src/main/java/se/simjarr/global/League.java
+++ src/main/java/se/simjarr/global/League.java
@@ -5,7 +5,7 @@ public enum League {
     LEGACY("Legacy"),
     HARDCORE_LEGACY("Hardcore Legacy"),
     STANDARD("Standard"),
-    HARDCORE("Harcore");
+    HARDCORE("Hardcore");
 
     private String urlName;
     private String displayName;
@@ -13,6 +13,13 @@ public enum League {
     League(String name) {
         this.displayName = name;
         this.urlName = name.replace(" ", "+");
+    }
+
+    public static League fromDisplayName(String displayName) {
+        for(League league : League.values()) {
+            if(league.getDisplayName().equals(displayName)) return league;
+        }
+        return null;
     }
 
     public String getUrlName() {
