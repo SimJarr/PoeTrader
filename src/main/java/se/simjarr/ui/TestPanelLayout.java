01c515b4d1c3107c2380adb36982fb1af4bd65ed
jaccohub <jacoblodenius@gmail.com>
2017-04-19 22:57:28 +0200

    testing jsoup posts

--- src/main/java/se/simjarr/ui/TestPanelLayout.java
+++ src/main/java/se/simjarr/ui/TestPanelLayout.java
@@ -1,71 +1,42 @@
 package se.simjarr.ui;
 
-import com.vaadin.ui.*;
-import se.simjarr.global.Currency;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
 import se.simjarr.global.ThreadLocalVariables;
-import se.simjarr.model.CurrencyTradeUrlBuilder;
-import se.simjarr.model.HttpRequestHandler;
-import se.simjarr.model.TradeOffer;
 
-import java.util.List;
+import java.io.IOException;
 
-import static se.simjarr.global.Currency.*;
+public class TestPanelLayout extends VerticalLayout {
 
-public class TestPanelLayout extends VerticalLayout{
-
-    private TextArea responseText;
-    private String url;
     private ThreadLocalVariables threadLocalVariables;
 
     public TestPanelLayout() {
         threadLocalVariables = ((ApplicationUI) UI.getCurrent()).getThreadLocalVariables();
-        addHeader();
-        addCurrencySelection();
-        addResponseText();
+
+        addButton();
     }
 
-    private void addHeader() {
-        Label header = new Label("poe-trade-application");
-        this.addComponent(header);
-    }
+    private void addButton() {
+        Button button = new Button("Test");
+        button.addClickListener(clickEvent ->  {
+            try {
+                Jsoup.connect("http://currency.poe.trade/shop?league=Hardcore+Legacy")
+                        .timeout(60000)
+                        .ignoreContentType(true)
+                        .method(Connection.Method.POST)
+                        .header("Content-Type", "application/x-www-form-urlencoded")
+                        .requestBody("league=Hardcore+Legacy&apikey=unitegasasiona&sell_currency=Orb+of+Fusing&sell_value=35&buy_value=121&buy_currency=Orb+of+Alchemy")
+                        .execute();
 
-    private void addCurrencySelection() {
-        HorizontalLayout formLayout = new HorizontalLayout();
-
-        CheckBoxGroup<Currency> have = new CheckBoxGroup<>();
-        have.setItems(ORB_OF_ALTERATION, ORB_OF_FUSING, ORB_OF_ALCHEMY, CHAOS_ORB);
-
-        CheckBoxGroup<Currency> want = new CheckBoxGroup<>();
-        want.setItems(ORB_OF_ALTERATION, ORB_OF_FUSING, ORB_OF_ALCHEMY, CHAOS_ORB);
-
-        Button send = new Button("Send");
-        send.addClickListener(clickEvent -> {
-            setRequestUrl(have, want);
-            List<TradeOffer> tradeOffers = HttpRequestHandler.fetchTradesFromUrl(url);
-            StringBuilder responseBuilder = new StringBuilder();
-            tradeOffers.forEach(x -> responseBuilder.append(x.getUsername()).append(", "));
-            //TODO this is now my testing-output
-            responseText.setValue(threadLocalVariables.getEstimatedValues().toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         });
-
-        formLayout.addComponents(have, want, send);
-        this.addComponent(formLayout);
+        this.addComponent(button);
     }
-
-    private void addResponseText() {
-        responseText = new TextArea("Response");
-        responseText.setEnabled(false);
-        responseText.setWidth("100%");
-        this.addComponent(responseText);
-    }
-
-    private void setRequestUrl(CheckBoxGroup<Currency> have, CheckBoxGroup<Currency> want) {
-        CurrencyTradeUrlBuilder urlBuilder = new CurrencyTradeUrlBuilder(threadLocalVariables.getSelectedLeague().getUrlName(), true);
-        urlBuilder.setHave(have.getSelectedItems().toArray(new Currency[have.getSelectedItems().size()]))
-                  .setWant(want.getSelectedItems().toArray(new Currency[want.getSelectedItems().size()]));
-        url = urlBuilder.build();
-    }
-
 }
 
 
