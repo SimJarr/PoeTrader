255d0880d9600b75081512cf2233bce3f90a2b51
Dewin <simon@temtron.se>
2017-04-20 12:26:49 +0200

    POST now works

--- src/main/java/se/simjarr/ui/TestPanelLayout.java
+++ src/main/java/se/simjarr/ui/TestPanelLayout.java
@@ -3,11 +3,12 @@ package se.simjarr.ui;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
-import org.jsoup.Connection;
-import org.jsoup.Jsoup;
 import se.simjarr.global.ThreadLocalVariables;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class TestPanelLayout extends VerticalLayout {
 
@@ -23,13 +24,21 @@ public class TestPanelLayout extends VerticalLayout {
         Button button = new Button("Test");
         button.addClickListener(clickEvent ->  {
             try {
-                Jsoup.connect("http://currency.poe.trade/shop?league=Hardcore+Legacy")
-                        .timeout(60000)
-                        .ignoreContentType(true)
-                        .method(Connection.Method.POST)
-                        .header("Content-Type", "application/x-www-form-urlencoded")
-                        .requestBody("league=Hardcore+Legacy&apikey=unitegasasiona&sell_currency=Orb+of+Fusing&sell_value=35&buy_value=121&buy_currency=Orb+of+Alchemy")
-                        .execute();
+
+                String url = "http://currency.poe.trade/shop?league=Hardcore+Legacy";
+                URL obj = new URL(url);
+                HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+                con.setRequestMethod("POST");
+                con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+
+                String urlParameters = "league=Hardcore+Legacy&apikey=unitegasasiona&sell_currency=Orb+of+Fusing&sell_value=11&buy_value=12&buy_currency=Orb+of+Alchemy";
+
+                con.setDoOutput(true);
+                DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+                wr.writeBytes(urlParameters);
+                wr.flush();
+                wr.close();
 
             } catch (IOException e) {
                 e.printStackTrace();
