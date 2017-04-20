0d680f687a9175f981778b9bd9bd6ae4bf6737cd
Dewin <simon@temtron.se>
2017-04-20 12:36:18 +0200

    bugfix

--- src/main/java/se/simjarr/ui/TestPanelLayout.java
+++ src/main/java/se/simjarr/ui/TestPanelLayout.java
@@ -5,8 +5,10 @@ import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 import se.simjarr.global.ThreadLocalVariables;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -32,13 +34,17 @@ public class TestPanelLayout extends VerticalLayout {
                 con.setRequestMethod("POST");
                 con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
 
-                String urlParameters = "league=Hardcore+Legacy&apikey=unitegasasiona&sell_currency=Orb+of+Fusing&sell_value=11&buy_value=12&buy_currency=Orb+of+Alchemy";
+                String urlParameters = "league=Hardcore+Legacy&apikey=unitegasasiona&sell_currency=Orb+of+Fusing&sell_value=17&buy_value=88&buy_currency=Orb+of+Alchemy";
 
                 con.setDoOutput(true);
                 DataOutputStream wr = new DataOutputStream(con.getOutputStream());
                 wr.writeBytes(urlParameters);
                 wr.flush();
                 wr.close();
+
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(con.getInputStream()));
+                in.close();
 
             } catch (IOException e) {
                 e.printStackTrace();
