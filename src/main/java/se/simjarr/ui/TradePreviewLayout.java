223b2c485c89c29e133982caef6a56e80dd44ec7
jaccohub <jacoblodenius@gmail.com>
2017-04-23 22:50:44 +0200

    added tradepreview section of the layout

--- src/main/java/se/simjarr/ui/TradePreviewLayout.java
+++ src/main/java/se/simjarr/ui/TradePreviewLayout.java
@@ -1,10 +1,11 @@
 package se.simjarr.ui;
 
-import com.vaadin.ui.Alignment;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.NativeSelect;
+import com.vaadin.data.HasValue;
+import com.vaadin.shared.ui.slider.SliderOrientation;
+import com.vaadin.ui.*;
 import se.simjarr.global.Currency;
+import se.simjarr.model.TradePreview;
+import se.simjarr.model.TradePreviewHandler;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,10 +15,61 @@ public class TradePreviewLayout extends HorizontalLayout {
     private OnDeleteUserOfferListener onDeleteUserOfferListener;
     private NativeSelect<String> sellCurrency;
     private NativeSelect<String> buyCurrency;
+    private List<TradePreview> tradePreviews;
+    private HorizontalLayout tradePreviewLayout;
+    private Slider slider;
 
     public TradePreviewLayout() {
         addDeleteButton();
         addCurrencySelection();
+    }
+
+    private void addSlider() {
+        if(slider != null) this.removeComponent(slider);
+        slider = new Slider();
+        slider.setMin(1);
+        slider.setMax(20);
+        slider.setWidth(100, Unit.PIXELS);
+        slider.setOrientation(SliderOrientation.HORIZONTAL);
+        slider.addValueChangeListener(valueChangeEvent -> {
+            double position = slider.getValue();
+            addTradePreview(tradePreviews.get((int)position-1));
+        });
+
+        this.addComponent(slider);
+        this.setComponentAlignment(slider, Alignment.BOTTOM_CENTER);
+    }
+
+    private void addTradePreview(TradePreview tradePreview) {
+        if(tradePreviewLayout != null) this.removeComponent(tradePreviewLayout);
+        tradePreviewLayout = new HorizontalLayout();
+
+        VerticalLayout positionLayout = new VerticalLayout();
+        positionLayout.setMargin(false);
+        Label positionHeader = new Label("Position");
+        Label position = new Label(String.valueOf(tradePreview.getPosition()));
+        positionLayout.addComponents(positionHeader, position);
+
+        VerticalLayout valueLayout = new VerticalLayout();
+        valueLayout.setMargin(false);
+        Label valueHeader = new Label("Value");
+        Label value = new Label(String.valueOf(tradePreview.getValue()));
+        valueLayout.addComponents(valueHeader, value);
+
+        VerticalLayout tradeDisplay = new VerticalLayout();
+        tradeDisplay.setMargin(false);
+        Label tradeDisplayHeader = new Label("Trade Display");
+        HorizontalLayout tradeDisplayLayout = new HorizontalLayout();
+        tradeDisplayLayout.setMargin(false);
+        Label sellValue = new Label(String.valueOf(tradePreview.getSellValue()));
+        Label colon = new Label(" : ");
+        Label buyValue = new Label(String.valueOf(tradePreview.getBuyValue()));
+        tradeDisplayLayout.addComponents(sellValue, colon, buyValue);
+        tradeDisplay.addComponents(tradeDisplayHeader, tradeDisplayLayout);
+
+        tradePreviewLayout.addComponents(positionLayout, valueLayout, tradeDisplay);
+        this.addComponent(tradePreviewLayout);
+        this.setComponentAlignment(tradePreviewLayout, Alignment.BOTTOM_CENTER);
     }
 
     private void addDeleteButton() {
@@ -35,22 +87,42 @@ public class TradePreviewLayout extends HorizontalLayout {
             allCurrencies.add(c.name());
         }
         sellCurrency = new NativeSelect<>("Sell Currency", allCurrencies);
-        buyCurrency = new NativeSelect<>("Buy Currency", allCurrencies);
         sellCurrency.setEmptySelectionAllowed(false);
+        sellCurrency.addValueChangeListener(valueChangeListener);
+
+        buyCurrency = new NativeSelect<>("Buy Currency", allCurrencies);
         buyCurrency.setEmptySelectionAllowed(false);
+        buyCurrency.addValueChangeListener(valueChangeListener);
 
         this.addComponents(sellCurrency, buyCurrency);
     }
 
-    public Currency getSellCurrency() {
+    private HasValue.ValueChangeListener<String> valueChangeListener = new HasValue.ValueChangeListener<String>() {
+        @Override
+        public void valueChange(HasValue.ValueChangeEvent<String> valueChangeEvent) {
+            if(buyCurrency.getValue() != null && sellCurrency.getValue() != null) {
+                if(!buyCurrency.getValue().equals(sellCurrency.getValue())) {
+                    TradePreviewHandler tradePreviewHandler = new TradePreviewHandler(getSellCurrency(), getBuyCurrency());
+                    setTradePreviews(tradePreviewHandler.getTradePreviews());
+                    addSlider();
+                }
+            }
+        }
+    };
+
+    private void setTradePreviews(List<TradePreview> tradePreviews) {
+        this.tradePreviews = tradePreviews;
+    }
+
+    private Currency getSellCurrency() {
         return Currency.fromName(sellCurrency.getValue());
     }
 
-    public Currency getBuyCurrency() {
+    private Currency getBuyCurrency() {
         return Currency.fromName(buyCurrency.getValue());
     }
 
-    public void setOnDeleteUserOfferListener(OnDeleteUserOfferListener onDeleteUserOfferListener) {
+    void setOnDeleteUserOfferListener(OnDeleteUserOfferListener onDeleteUserOfferListener) {
         this.onDeleteUserOfferListener = onDeleteUserOfferListener;
     }
 
