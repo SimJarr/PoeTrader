65cee80c7a86f617c0513c07c860bebcfccd814b
jaccohub <jacoblodenius@gmail.com>
2017-04-24 22:48:23 +0200

    bugfix for special cases where buyvalue is alot higher than sellvalue

--- src/main/java/se/simjarr/model/TradePreviewHandler.java
+++ src/main/java/se/simjarr/model/TradePreviewHandler.java
@@ -11,39 +11,55 @@ public class TradePreviewHandler {
     private double firstPositionPricePerUnit;
     private Currency sellCurrency;
     private Currency buyCurrency;
+    private boolean moreDecimals;
 
     public TradePreviewHandler(Currency sellCurrency, Currency buyCurrency) {
         tradeOffers = TradeBank.selectTrades(buyCurrency, sellCurrency, null, null);
+        moreDecimals = placeholderName();
+        firstPositionPricePerUnit = findFirstPositionPricePerUnit();
+
         this.sellCurrency = sellCurrency;
         this.buyCurrency = buyCurrency;
-        firstPositionPricePerUnit = findFirstPositionPricePerUnit();
     }
 
     public List<TradePreview> getTradePreviews() {
         List<TradePreview> tradePreviews = new ArrayList<>();
-        for(double i = firstPositionPricePerUnit; i < firstPositionPricePerUnit + 1; i += 0.05) {
-            i = Math.round(i*100.0)/100.0;
-            int sellValue = calculateSellValue(i);
-            int buyValue = (int)(sellValue * i);
+        double x = (moreDecimals) ? 0.01 : 1.00;
+        double increment = x / 20;
+        double start = firstPositionPricePerUnit;
+        double stop = start + x;
 
-            TradeOffer temp = new TradeOffer(null, sellCurrency.getStringValue(), String.valueOf(sellValue), buyCurrency.getStringValue(), String.valueOf(buyValue), null, "");
-
-            TradePreview tradePreview = new TradePreview(findPosition(i), sellValue, buyValue, temp.calculateTradeValue()/sellValue * -1);
+        for (double i = start; i < stop; i += increment) {
+            TradePreview tradePreview;
+            if (moreDecimals) {
+                i = Math.round(i * 10000.0) / 10000.0;
+                int buyValue = calculateSellValue(i);
+                int sellValue = (int) (buyValue / i);
+                TradeOffer temp = new TradeOffer(null, sellCurrency.getStringValue(), String.valueOf(sellValue), buyCurrency.getStringValue(), String.valueOf(buyValue), null, "");
+                tradePreview = new TradePreview(findPosition(i), sellValue, buyValue, temp.calculateTradeValue() / buyValue * -1);
+            } else {
+                i = Math.round(i*100.0)/100.0;
+                int sellValue = calculateSellValue(i);
+                int buyValue = (int)(sellValue * i);
+                TradeOffer temp = new TradeOffer(null, sellCurrency.getStringValue(), String.valueOf(sellValue), buyCurrency.getStringValue(), String.valueOf(buyValue), null, "");
+                tradePreview = new TradePreview(findPosition(i), sellValue, buyValue, temp.calculateTradeValue()/sellValue * -1);
+                tradePreviews.add(tradePreview);
+            }
             tradePreviews.add(tradePreview);
         }
         return tradePreviews;
     }
 
     private int calculateSellValue(double pricePerUnit) {
-        int hundredth = (int)((pricePerUnit * 100) % 10);
-        if(hundredth == 5) {
+        int hundredth = (int) ((pricePerUnit * 100) % 10);
+        if (hundredth == 5) {
             return 20;
         } else {
-            int tenth = (int)((pricePerUnit * 10) % 10);
-            if(tenth == 0 && hundredth == 0) return 1;
-            if(tenth == 5) return 2;
-            else{
-                if(tenth % 2 == 0) return 5;
+            int tenth = (int) ((pricePerUnit * 10) % 10);
+            if (tenth == 0 && hundredth == 0) return 1;
+            if (tenth == 5) return 2;
+            else {
+                if (tenth % 2 == 0) return 5;
                 else return 10;
             }
         }
@@ -51,8 +67,8 @@ public class TradePreviewHandler {
 
     private int findPosition(double pricePerUnit) {
         int position = 1;
-        for(TradeOffer tradeOffer : tradeOffers) {
-            if(pricePerUnit >= tradeOffer.getPricePerUnit()) position++;
+        for (TradeOffer tradeOffer : tradeOffers) {
+            if (pricePerUnit >= tradeOffer.getPricePerUnit()) position++;
             else break;
         }
         return position;
@@ -62,20 +78,31 @@ public class TradePreviewHandler {
         TradeOffer currentFirstPosition = tradeOffers.get(0);
         double currentPricePerUnit = currentFirstPosition.getPricePerUnit();
 
-        return findNextLowerPrice(currentPricePerUnit);
+        int relevantDecimal = (moreDecimals) ? 4 : 2;
+        return findNextLowerPrice(currentPricePerUnit, relevantDecimal);
     }
 
-    private double findNextLowerPrice(double currentPricePerUnit) {
-        int currentHundredth = (int)(currentPricePerUnit * 100) % 10;
-        int nextHundredth = (currentHundredth > 5 || currentHundredth == 0) ? 5 : 0;
+    private double findNextLowerPrice(double currentPricePerUnit, int relevantDecimal) {
+        int currentDecimal = findNthDecimal(currentPricePerUnit, relevantDecimal);
+        int nextDecimal = (currentDecimal > 5 || currentDecimal == 0) ? 5 : 0;
 
-        double nextLowerPrice = (int)(currentPricePerUnit * 10);
-        nextLowerPrice *= 10;
-        nextLowerPrice -= nextHundredth;
-        nextLowerPrice /= 100;
+        String nextLowerPrice = (int) currentPricePerUnit + ".";
+        for (int i = 1; i < relevantDecimal; i++) {
+            nextLowerPrice += findNthDecimal(currentPricePerUnit, i);
+        }
+        nextLowerPrice += nextDecimal;
+        return Double.parseDouble(nextLowerPrice);
+    }
 
-        nextLowerPrice = Math.round(nextLowerPrice*100.0)/100.0;
-        return nextLowerPrice;
+    // TODO: rename
+    private boolean placeholderName() {
+        double pricePerUnit = tradeOffers.get(0).getPricePerUnit();
+        if (pricePerUnit - (int) pricePerUnit == 0) return false;
+        return (findNthDecimal(pricePerUnit, 1) == 0);
+    }
+
+    private int findNthDecimal(double num, int decimal) {
+        return (int) (Math.abs(num) * Math.pow(10, decimal)) % 10;
     }
 }
 
